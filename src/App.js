dade98da0b24e9dfc283a4f41d3d90c80e209076
madhuri <madhuri.djs3@gmail.com>
2021-04-16 23:43:28 +0530

    enables button if user login details are entered

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   const [show, setShow] = useState(false);
   const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const isEnabled = user.length > 0 && password.length > 0;
   return (
     <div className="App">
       <div className= "user_login">
@@ -18,9 +20,14 @@ function App() {
           type= "password"
           className= "password"
           placeholder= "Enter PassWord"
+          onChange= {event => setPassword(event.target.value) }
         />
-        {}
+        {isEnabled ?
+        <div>
         <button className= "show-modal" onClick= {() => setShow(true)}>Login</button>
+        </div> : <div>
+        <button disabled = {!isEnabled} className= "disable-showmodal" onClick= {() => setShow(true)}>Login</button> 
+        </div>}
         <Modal user= {user} onClose= {() => setShow(false)} show= {show} />
       </div>
     </div>
